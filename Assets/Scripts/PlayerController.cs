849ccc60b97d53f9002e2be472429c4e05651e30
Trương Quang Vinh <vt3839796@gmail.com>
2024-08-10 21:04:31 +0700

    Adds basic motion to the prototype

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,8 +7,16 @@ public class PlayerController : MonoBehaviour
 {
     public Joystick joystick;
     private float playerSpeed = 0.1f;
+    private float jumpSpeed = 5.0f;
+    private Rigidbody2D playerRb;
+
+    private void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+    }
+
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
         // if (Input.touchCount > 0)
         // {
@@ -17,9 +26,29 @@ public class PlayerController : MonoBehaviour
         //     transform.position = touchPos;
         // }
 
-        float horizontalMove = joystick.Horizontal * playerSpeed;
 
-        transform.position = new Vector3(transform.position.x + horizontalMove, transform.position.y);
-        // transform.position.x += horizontalMove;
+        PlayerMove();
+
+    }
+
+    public void PlayerMove()
+    {
+        // If moved horizontally, it approaches 0
+        if (joystick.Horizontal <= 0.2f && joystick.Horizontal >= -0.2f) return; // Not move
+
+        // Move the player horizontal
+        float horizontalMove = joystick.Horizontal * playerSpeed;
+        playerRb.AddForce(new Vector2(horizontalMove, 0.0f), ForceMode2D.Impulse);
+    }
+
+    public void PlayerJump()
+    {
+        // User request jump
+        playerRb.AddForce(new Vector2(0.0f, jumpSpeed), ForceMode2D.Impulse);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        
     }
 }
