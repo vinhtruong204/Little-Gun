68a55fb105c29e464958feaa7e658221ed0ddb76
Trương Quang Vinh <vt3839796@gmail.com>
2024-08-14 21:22:18 +0700

    Adjust character movements and edit MainScene

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,50 +5,94 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Variables relate joystick
     public Joystick joystick;
-    private float playerSpeed = 0.1f;
-    private float jumpSpeed = 5.0f;
+    private float boundTriggerMove = 0.2f;
+
+    // Player move
+    private float playerSpeed = 3.0f;
+    private float jumpPower = 5.0f;
+    private bool isOnground;
+    private int jumpCount;
+
+    // Player component
     private Rigidbody2D playerRb;
+    private BoxCollider2D boxCollider2D;
+    private Vector3 defaultPos = new Vector3(-2.62f, -0.4f, 0);
 
     private void Start()
     {
         playerRb = GetComponent<Rigidbody2D>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     private void FixedUpdate()
     {
-        // if (Input.touchCount > 0)
-        // {
-        //     Touch touch = Input.GetTouch(0);
+        // Move the character
+        PlayerMoveHorizontal();
 
-        //     Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
-        //     transform.position = touchPos;
-        // }
-
-
-        PlayerMove();
+        // Adjust box collider when player is falling
+        HandleFall();
 
     }
 
-    public void PlayerMove()
+    private void HandleFall()
+    {
+        // Check player is jumping or falling
+        boxCollider2D.enabled = playerRb.velocity.y <= 0 ? true : false;
+    }
+
+    private void PlayerMoveHorizontal()
     {
         // If moved horizontally, it approaches 0
-        if (joystick.Horizontal <= 0.2f && joystick.Horizontal >= -0.2f) return; // Not move
+        if (joystick.Horizontal <= boundTriggerMove && joystick.Horizontal >= -boundTriggerMove) return; // Not move
 
-        // Move the player horizontal
-        float horizontalMove = joystick.Horizontal * playerSpeed;
-        playerRb.AddForce(new Vector2(horizontalMove, 0.0f), ForceMode2D.Impulse);
+        // Set velocity for rigidbody
+        Vector2 newVelocityRb = new Vector2(joystick.Horizontal * playerSpeed, playerRb.velocity.y);
+        playerRb.velocity = newVelocityRb;
     }
+
 
     public void PlayerJump()
     {
-        // User request jump
-        playerRb.AddForce(new Vector2(0.0f, jumpSpeed), ForceMode2D.Impulse);
+        // Check double jump
+        if (jumpCount >= 2)
+        {
+            return;
+        }
+
+        // Set up for double jump
+        if (jumpCount < 2 || isOnground)
+        {
+            // User request jump
+            playerRb.velocity = new Vector2(playerRb.velocity.x, jumpPower);
+
+            // Reset onGround boolean
+            isOnground = false;
+
+            // Increase jump count
+            jumpCount++;
+        }
+    }
+
+    public void ResetPos()
+    {
+        // Reset position to default when player fall into the abyss
+        transform.position = defaultPos;
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            jumpCount = 0;
+            isOnground = true;
+            if (other.gameObject.transform.position.y < transform.position.y)
+            {
+                boxCollider2D.enabled = false;
+
+            }
+        }
     }
 }
